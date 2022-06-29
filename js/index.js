0d855fa641d314f30133592fb6a33920d96cd31f
Najmus Sakib <92445715+najmusskb@users.noreply.github.com>
2022-06-29 23:22:10 +0600

    _

--- js/index.js
+++ js/index.js
@@ -1,3 +1,32 @@
+// java script object notation 
+// JSON
 const User = { id: 11, name: 'gorib Amir', job: 'actor' }
 
-console.log(User);
+
+const stringified = JSON.stringify(User);
+
+
+// console.log(User);
+
+// console.log(stringified);
+
+const shop = {
+
+    name: 'alia store',
+    address: 'kocuuuuuu',
+    profit: 15000,
+    products: ['laptop', 'mobile', 'pepsi'],
+    owner: {
+        name: 'alia batt',
+        Profession: 'actor'
+
+    },
+    isExpansive: false
+}
+
+const ShopStringgfied = JSON.stringify(shop);
+// console.log(shop);
+console.log(ShopStringgfied);
+
+const converted = JSON.parse(ShopStringgfied);
+console.log(converted);
