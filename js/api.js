9aca86a2dba5d34eb51e9e95902867c3d85ba0bf
Najmus Sakib <92445715+najmusskb@users.noreply.github.com>
2022-06-29 23:41:03 +0600

    _

---
+++ js/api.js
@@ -0,0 +1,18 @@
+// // fetch mane hocce kichu tule niya asa
+
+
+// fetch('https://jsonplaceholder.typicode.com/todos/1')
+//     .then(response => response.json())
+//     .then(json => console.log(json))
+
+// load data
+
+function loadData() {
+
+    fetch('https://jsonplaceholder.typicode.com/todos/1')
+        .then(response => response.json())
+        .then(json => console.log(json))
+
+
+
+}
