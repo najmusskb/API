d2b2110a9094e815bc23c702dd8691262d317e46
Najmus Sakib <92445715+najmusskb@users.noreply.github.com>
2022-06-29 23:59:41 +0600

    _

--- js/api.js
+++ js/api.js
@@ -14,5 +14,32 @@ function loadData() {
         .then(json => console.log(json))
 
 
+}
+
+
+
+function loadUsers() {
+
+    fetch('https://jsonplaceholder.typicode.com/users')
+        .then(response => response.json())
+        .then(json => displayUsers(json))
+}
+
+
+
+function postUsers() {
+
+    fetch('https://jsonplaceholder.typicode.com/posts')
+        .then(response => response.json())
+        .then(data => console.log(data))
+}
+
+
+// automatic call --------------  postUsers()
+
+
+function displayUsers(data) {
+
+    console.log(data);
 
 }
